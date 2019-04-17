5ec3af3e096774f4b583475d094942212b86720f
Soumaya Kochtebene <soumaya.kochtebene@particeep.com>
2019-04-17 18:37:30 +0200

    added main

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ _DateTime_: the date of the request in the iso 8601 format. e.g: 2015-10-19T11:0
 
 _Authorization_: a hash formed with the api keys and the DateTime.
 
-This is an example of implementation in java, to build a authorization header.
+This is an example of implementation in java, to build an authorization header.
 
 **Usage**
+
 Use the method buildAuthorizationHeader to get an accepted authorization header using your apikey and apisecret. 
 
 Example:
@@ -23,6 +24,6 @@ _date_time_: "2019-04-16T11:19:33Z"
 
 `buildAuthorizationHeader("2019-04-16T11:19:33Z", "ce657875-613a-4636-ad53-a2f127f7811e", "1c61cb5a-294e-4085-845a-282dc479990d")`
  
- We get this string as result: 
+ We get this string as a result: 
  
-**PTP:693fb0bc-5433-4b88-9c68-9f6ab024eb50:MjJiYTEwZTVmZTljZjMzNmE4MzlkMzE2Y2Y4OGIxZmEzNzY3Y2M2MA==**
+**PTP:ce657875-613a-4636-ad53-a2f127f7811e:ZDhhZWMyZDU0NzRmMjBhMWUxYzY2OGFmZWJlOGE4ZmZiNWM2MjZiMQ==**
--- src/Authentication.java
+++ src/Authentication.java
@@ -3,6 +3,7 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 public class Authentication {
+
     static String buildAuthorizationHeader(String date, String apiKey, String apiSecret) throws UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException {
 
         String toSign = apiSecret + apiKey + date;
@@ -13,5 +14,15 @@ public class Authentication {
         String sign = new String(hexChars).toLowerCase();
         String signature = Crypto.encodeBase64(sign);
         return "PTP:" + apiKey + ":" + signature;
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(buildAuthorizationHeader("2019-04-16T11:19:33Z", "ce657875-613a-4636-ad53-a2f127f7811e", "1c61cb5a-294e-4085-845a-282dc479990d"));
+            // The result is "PTP:ce657875-613a-4636-ad53-a2f127f7811e:ZDhhZWMyZDU0NzRmMjBhMWUxYzY2OGFmZWJlOGE4ZmZiNWM2MjZiMQ=="
+
+        } catch (UnsupportedEncodingException | InvalidKeyException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
